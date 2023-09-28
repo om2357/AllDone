ccf55c7e5c0bb4a52ca2dd5f43c7e5ad4f3c4100
OmKumar <omk2357@gmail.com>
2023-09-28 16:29:11 +0530

    going on

--- BinarrySearch/ReStart.cpp
+++ BinarrySearch/ReStart.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int search(int arr[], int size, int key)
 {
     int start = 0;
     int end = size - 1;
-    int mid = (start + end)/ 2;
+    int mid = (start + end) / 2;
 
-    while(start<=end)
+    while (start <= end)
     {
-        if(arr[mid] == key)
+        if (arr[mid] == key)
         {
             return mid;
         }
-        if(key > arr[mid])
+        if (key > arr[mid])
         {
             start = mid + 1;
         }
@@ -20,18 +20,17 @@ int search(int arr[], int size, int key)
         {
             start = mid - 1;
         }
-        mid = (start+end)/2;
+        mid = (start + end) / 2;
     }
     return -1;
 }
 int main()
 {
-    int arr[6] = {2,12,13,23,31,43};
-    cout<<"enter your key :- "<<endl;
+    int arr[6] = {2, 12, 13, 23, 31, 43};
+    cout << "enter your key :- " << endl;
     int key;
-    cin>>key;
-    cout<<"your key index is --> "<<search(arr,6,key)<<endl;
-    
+    cin >> key;
+    cout << "your key index is --> " << search(arr, 6, key) << endl;
 
     return 0;
 }
--- BinarrySearch/binary.cpp
+++ BinarrySearch/binary.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int search(int arr[], int size, int key)
 {
     int start = 0;
-    int end = size-1;
-    int mid = start + (end - start)/2;
+    int end = size - 1;
+    int mid = start + (end - start) / 2;
 
-    while(start<=end)
+    while (start <= end)
     {
-        if(arr[mid]==key)
+        if (arr[mid] == key)
         {
             return mid;
         }
-        if(key>arr[mid])
+        if (key > arr[mid])
         {
             start = mid + 1;
         }
@@ -20,25 +20,25 @@ int search(int arr[], int size, int key)
         {
             end = mid - 1;
         }
-        mid = start + (end - start)/2;        
+        mid = start + (end - start) / 2;
     }
     return -1;
 }
 int main()
 {
-    
-    cout<<"Enter the size of array -: "<<endl;
+
+    cout << "Enter the size of array -: " << endl;
     int size;
-    cin>>size;
+    cin >> size;
     int arr[10];
-    for(int i = 0; i<size; i++)
+    for (int i = 0; i < size; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    cout<<"enter the value of Array :-";
+    cout << "enter the value of Array :-";
     int key;
-    cout<<"Enter your Key :- "<<endl;
-    cin>>key;
-    cout<<"your index number is --> "<<search(arr,size,key)<<endl;
+    cout << "Enter your Key :- " << endl;
+    cin >> key;
+    cout << "your index number is --> " << search(arr, size, key) << endl;
     return 0;
 }
