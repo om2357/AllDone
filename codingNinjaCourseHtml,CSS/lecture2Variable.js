5bb1a35e125fe7e95000b093be45ee26f3e97b8a
OmKumar <omk2357@gmail.com>
2023-09-28 16:17:59 +0530

    start

---
+++ codingNinjaCourseHtml,CSS/lecture2Variable.js
@@ -0,0 +1,10 @@
+console.log('OnseAgain');
+let age = 10;
+console.log(age);// Variables are Dynamicaly typ
+
+age = 23;
+console.log(age);
+
+// let, const, var --> this are types of variable 
+
+alert("Helloo");
