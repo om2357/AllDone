781378816a062f49eb73a2e391bc79595c0f6d48
OmKumar <omk2357@gmail.com>
2023-10-20 22:47:09 +0530

    JavaScriptGoingOn :)

---
+++ codingNinjaCourseHtml,CSS/another.js
@@ -0,0 +1,25 @@
+const parent = document.querySelector(".parent");
+console.log(parent);
+console.log(parent.children);
+
+
+console.log(alert);
+console.log(parent.children[1]);
+console.log(parent.children[1].innerHTML);
+
+
+// for(let i = 0; i< parent.children.length; i++)
+// {
+//     console.log(parent.children[i].innerHTMl);
+
+// }
+// parent.children[1].style.color = "orange";
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+const dayOne = document.querySelector('.day')
+
+console.log(dayOne);
+console.log(dayOne.parentElement);
+console.log(dayOne.nextElementSibling);
+
+
