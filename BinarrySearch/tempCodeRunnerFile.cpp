c6a680e15c67c86531719558b19a24e8b1cca982
Om Kumar <omk2357@gmail.com>
2025-07-13 21:01:25 +0530

    done

---
+++ BinarrySearch/tempCodeRunnerFile.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int search(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+int main()
+{
+
+    cout << "Enter the size of array -: "
